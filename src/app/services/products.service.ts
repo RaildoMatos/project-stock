fa84582ec4fc82b2a8d5faeafd92d37e89a045f5
raildomatos <raildom437@gmail.com>
2023-08-21 19:48:22 -0300

    .

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../models/product';
+import { Product } from '../models/Product';
 import { Page } from '../models/page';
 
 @Injectable({
